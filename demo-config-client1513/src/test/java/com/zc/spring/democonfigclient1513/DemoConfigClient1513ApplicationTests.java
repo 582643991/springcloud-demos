ee97968f95b4141ec2560794d1ba1fb030c8b5f2
582643991 <582643991@qq.com>
2018-05-30 15:43:01 +0800

    提交源码

---
+++ demo-config-client1513/src/test/java/com/zc/spring/democonfigclient1513/DemoConfigClient1513ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zc.spring.democonfigclient1513;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoConfigClient1513ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
